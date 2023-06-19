d72d114391ac5c4d371c4c517df864b75c70b52e
Rcatarata <69015151+Rcatarata@users.noreply.github.com>
2023-06-19 14:58:30 -0400

    installing packages
    installing packages

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+
+const port = process.env.PORT
+
+app.listen(port, () =>{
+    console.log(`listening on ${port}`)
+})
+
